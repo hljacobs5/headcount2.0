0324d6c9a10b5e6e63b2c87e327bf226eaca7405
Haley Jacobs <haleyljacobs@gmail.com>
2018-10-02 15:47:21 -0600

    Iteration 0 completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React, { Component } from 'react';
+import DistrictRepository from './helper.js'
+import kinderData from './data/kindergartners_in_full_day_program.js'
+
 import './App.css';
 
 class App extends Component {
+	constructor() {
+		super();
+		this.state = {
+			district: new DistrictRepository(kinderData)
+		}
+	}
+
+	componentDidMount() {
+
+	}
   render() {
     return (
-      <div>Welcome To Headcount 2.0</div>
+      <div>Headcount 2.0</div>
     );
   }
 }
--- src/helper.js
+++ src/helper.js
@@ -1,3 +1,26 @@
+import React from 'react'
+import kinderData from './data/kindergartners_in_full_day_program.js';
+
 export default class DistrictRepository {
+	constructor(data) {
+		this.stats = this.removeDuplicates(kinderData)
+	}
+
+	removeDuplicates = (kinderData) => {
+		return kinderData.reduce((dataObject, district) => {
+		let key = district.Location.toUpperCase()
+
+		if (!dataObject[key]) {
+			dataObject[key] = {
+				location: district.Location,
+				stats: {}
+			}
+
+		}
+
+		return dataObject
+		}, {})
+
+	}
 
 }
